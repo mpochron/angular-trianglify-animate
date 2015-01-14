ecc2876c1fc14e5a5e2aa4237648f01b976ad276
Maciej <machei.dehogo@gmail.com>
2015-01-14 03:09:54 +0100

    +updated things
    +fixed polygon
    +added attributes
    +added example

--- trianglify.animate.js
+++ trianglify.animate.js
@@ -1,17 +1,32 @@
-// Trianglify Animate
-// https://github.com/machei/angular-trianglify-animate
-// @author: machei
-// @thanks: grohlf & empathetic-alligator 
-
+/* 
+ * Angular Trianglify Animate
+ * github: https://github.com/machei/angular-trianglify-animate
+ * @author: machei
+ * @thanks: grohlf & empathetic-alligator 
+ */
+ 
 function scale(valueIn, baseMin, baseMax, limitMin, limitMax) {
     return ((limitMax - limitMin) * (valueIn - baseMin) / (baseMax - baseMin)) + limitMin;
 }
-var app = angular.module('TriApp', []);
 
-app.directive("ngTrianglifyAnimate", ["$interval", "$compile", function($interval, $compile) {
-    return {
-		restrict:'A',
-		link: function($scope, $element, $attrs) {
+angular.module('moduleTrianglifyAnimate', [])
+.constant('configTrianglifyAnimate', {
+	speed:1, 
+	vectorX: 1, 
+	vectorY: 1, 
+	baseMax: 0, 
+	baseMin: 500, 
+	theta: 0.4,
+	pathPassSymbols : 'MZLHmzlh'
+})
+.directive("ngTrianglifyAnimate", ["$compile", function($compile) {
+	return {
+		restrict:'EA',
+		scope:{
+			interval: '='
+		},
+		controller: ['$scope', '$element', '$attrs', '$interval', 'configTrianglifyAnimate', function ($scope, $element, $attrs, $interval, configTrianglifyAnimate) {
+			// Replace img with svg
 			if($element[0].tagName === "IMG" && window.File && window.FileList && window.FileReader){
 				var $e = $element[0] , $p = $element[0].parentNode;
 				var xhr = new XMLHttpRequest();
@@ -44,50 +59,56 @@ app.directive("ngTrianglifyAnimate", ["$interval", "$compile", function($interva
 				return true;
 			}
 			
+			// Attrs			
+			angular.forEach(['speed', 'vectorX', 'vectorY', 'baseMax', 'baseMin', 'theta'], function( key, index ) {
+				$scope[key] = angular.isDefined($attrs[key]) ? (index < 6 ? $interpolate($attrs[key])($scope.$parent) : $scope.$parent.$eval($attrs[key])) : configTrianglifyAnimate[key];
+			});
+
+			// Init Shapes
 			var polygons = $element[0].getElementsByTagName('polygon');
 			var polys = [];
 			for(var i=0; i<polygons.length; i++){
-				polygon = polygons[i];
-				p = polygon.points;
-				polygon = angular.element(polygon);
-				polygon.attr("ng-class","{{point=points["+i+"]}}");
-				polygon.attr("ng-attr-points","{{point.x1}},{{point.y1}} {{point.x2}},{{point.y2}} {{point.x3}},{{point.y3}}");
-				$compile(polygon)($scope);
-				polys[i] = {x1:p[0].x ,y1:p[0].y, x2:p[1].x, y2:p[1].y, x3:p[2].x , y3:p[2].y};
+				polys[i] = polygons[i].points;
 			}
 			
 			var el_paths = $element[0].getElementsByTagName('path');
 			var paths = [], path;
+
 			for(var i=0; i<el_paths.length; i++){
-				path = paths[i] = el_paths[i].pathSegList;
-				path = angular.element(path);
-				$compile(path)($scope);
+				paths[i] = el_paths[i].pathSegList;
 			}
 			
-			$scope.points = polys;
+			$scope.polys = polys;
 			$scope.paths = paths;
-			$scope.pathPassSymbols = "MZLHV"
 			$scope.theta = 0;
-			$scope.animate = $interval(function() {
+			
+			var animate = function(){
+			
 				// polygons
-				for(var i = 0; i < $scope.points.length; i++) {
-					var point = $scope.points[i];
-					point.y1 = point.y1 + Math.sin($scope.theta + scale(point.x1, 0, 500, 0, 2 * Math.PI)) * 0.5;
-					point.y2 = point.y2 + Math.sin($scope.theta + scale(point.x2, 0, 500, 0, 2 * Math.PI)) * 0.5;
-					point.y3 = point.y3 + Math.sin($scope.theta + scale(point.x3, 0, 500, 0, 2 * Math.PI)) * 0.5;
+				for(var i = 0; i < $scope.polys.length; i++) {
+					$poly = $scope.polys[i];
+					for(var q=0; q < $poly.length; q++){
+						$poly[q].y = $poly[q].y + Math.sin($scope.theta + scale($poly[q].x, 0, 500, 0, 2 * Math.PI)) * 0.5;
+					}
 				}
+				
 				// path
 				for(var i = 0; i < $scope.paths.length; i++) {
 					$path = $scope.paths[i];
 					for(var q=0; q<$path.length; q++){
-						if($scope.pathPassSymbols.indexOf($path[q].pathSegTypeAsLetter) > -1){
+						if(configTrianglifyAnimate.pathPassSymbols.indexOf($path[q].pathSegTypeAsLetter) > -1){
 							$path[q].y = $path[q].y + Math.sin($scope.theta + scale($path[q].x, 0, 500, 0, 2 * Math.PI)) * 0.5;
+						}else{
+							//console.log($path[q].pathSegTypeAsLetter);
 						}
 					}
 				}	
+				
 				// todo: circle, rect, ellipse, line, polyline
 				$scope.theta += 0.04;
-			}, 10);
-		}
-	};
+			};
+			$scope.animate = $interval(function(){animate()}, 10/$scope.speed, false);
+
+		}]
+};
 }]);
